11352dc5a059b8a10a465b7d119a45d895715cd0
Vangeepuram Sreekesari <58507604+sreekesari-vangeepuram@users.noreply.github.com>
2020-09-09 00:41:23 +0530

    Update connect.sh

--- overthewire/bandit/connect.sh
+++ overthewire/bandit/connect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # `connect.sh` connect to the remote machine to complete a certain level of overthewire/bandit
-ssh bandit${1}@bandit.labs.overthewire.org -p 2220 ${2}
+ssh bandit${1}@bandit.labs.overthewire.org -p 2220 # ${2} <-- This was added later. You can find this edit in later challenges!
